725c481437d823a2da492d57fe727a13b4a5b1c7
Riziq fattah & Salma Aulia <kupingkecil.riz@gmail.com>
2020-04-09 15:43:39 +0800

    Stateless Widget

---
+++ README.md
@@ -0,0 +1,5 @@
+# Exercise 1 : Basic State Management with Stateful Widgets
+
+## SCSJ3623, Mobile Application Programming
+
+## Semester 2, 2019/20202
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+// Mobile Application Programming (SCSJ3623)
+// Semester 2, 2019/2020
+
+// Exercise 1: Basic State Management with Stateful Widgets
+
+// Write your name and your partner's.
+// Only one submission is required per pair
+// Submit only the source code file (main.dart)
+
+//! Name 1:  ......
+//! Name 2:  ......
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'MAP Exercise 1',
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FlutterLogo(size: 150.0),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: Text('Color'),
+        icon: Icon(Icons.color_lens),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 150.0,
+        child: Slider(
+          min: 0.0,
+          max: 300.0,
+          value: 150.0,
+          onChanged: (newValue) {},
+        ),
+      ),
+    );
+  }
+}
